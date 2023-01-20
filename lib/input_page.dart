ee304cbeedab9b4c3c4da32c1fe7f34ba2f1ac81
Max Soe <maxsoe@gmail.com>
2023-01-20 23:20:34 +0400

    126. The GestureDetector Widget

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -4,6 +4,7 @@ import 'reusable_card.dart';
 
 const double bottomContainerHeight = 80;
 const Color activeCardColour = Color(0xFFFFE0B2); //orange[100]
+const Color inactiveCardColour = Color(0xffFFECB3); //amber[100]
 const Color bottomContainerColour = Color(0xFF6002EE); //purple[500]
 // final Color bottomContainerColour = Theme.of(context).colorScheme.secondary;
 
@@ -13,6 +14,35 @@ class InputPage extends StatefulWidget {
 }
 
 class _InputPageState extends State<InputPage> {
+  Color maleCardColour = inactiveCardColour;
+  Color femaleCardColour = inactiveCardColour;
+
+  void updateColour(String gender) {
+    if (gender == 'male') {
+      debugPrint('Male card was tapped');
+      if (maleCardColour == inactiveCardColour) {
+        maleCardColour = activeCardColour;
+        femaleCardColour = inactiveCardColour;
+        debugPrint('Male card is now active');
+      } else {
+        maleCardColour = inactiveCardColour;
+        debugPrint('Male card is now inactive');
+      }
+    }
+
+    if (gender == 'female') {
+      debugPrint('Female card was tapped');
+      if (femaleCardColour == inactiveCardColour) {
+        femaleCardColour = activeCardColour;
+        maleCardColour = inactiveCardColour;
+        debugPrint('Female card is now active');
+      } else {
+        femaleCardColour = inactiveCardColour;
+        debugPrint('Female card is now inactive');
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     // final Color bottomContainerColour = Theme.of(context).colorScheme.secondary;
@@ -30,20 +60,35 @@ class _InputPageState extends State<InputPage> {
                 child: Row(
                   children: [
                     Expanded(
-                      child: ReusableCard(
-                        colour: activeCardColour,
-                        cardChild: GenderCardContents(
-                          icon: Icons.male,
-                          label: 'Male',
+                      child: GestureDetector(
+                        // onTap: () => (debugPrint('Male card was tapped')),
+                        onTap: () {
+                          setState(() {
+                            updateColour('male');
+                          });
+                        },
+                        child: ReusableCard(
+                          colour: maleCardColour,
+                          cardChild: GenderCardContents(
+                            icon: Icons.male,
+                            label: 'Male',
+                          ),
                         ),
                       ),
                     ),
                     Expanded(
-                      child: ReusableCard(
-                        colour: activeCardColour,
-                        cardChild: GenderCardContents(
-                          icon: Icons.female,
-                          label: 'Female',
+                      child: GestureDetector(
+                        onTap: () {
+                          setState(() {
+                            updateColour('female');
+                          });
+                        },
+                        child: ReusableCard(
+                          colour: femaleCardColour,
+                          cardChild: GenderCardContents(
+                            icon: Icons.female,
+                            label: 'Female',
+                          ),
                         ),
                       ),
                     ),
